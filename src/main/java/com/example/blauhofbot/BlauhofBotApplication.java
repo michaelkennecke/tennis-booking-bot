d89e249647811db19263270b708f6e25198fbd39
Michael Kennecke <mkennecke@trustdble.com>
2022-11-26 10:51:09 +0100

    Initial commit

---
+++ src/main/java/com/example/blauhofbot/BlauhofBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.blauhofbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlauhofBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlauhofBotApplication.class, args);
+    }
+
+}
