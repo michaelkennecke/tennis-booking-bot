fd923e1ec601e267189be067a65fbf2a5ad5904f
Michael Kennecke <mkennecke@trustdble.com>
2022-12-27 16:16:25 +0100

    Add properties file for email and password; add retry mechanism when booking failed

--- src/main/java/com/example/blauhofbot/Controller/BotController.java
+++ src/main/java/com/example/blauhofbot/Controller/BotController.java
@@ -4,9 +4,7 @@ import com.example.blauhofbot.Service.BotService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
 import java.util.*;
 
 @RestController
@@ -15,6 +13,8 @@ public class BotController {
 
     private BotService botService;
     private HashMap<String, TimerTask> courtBookingTasks;
+    private LocalTime BOOKING_START_TIME = LocalTime.of(8, 00, 00);
+    private LocalTime BOOKING_END_TIME = LocalTime.of(9, 30, 00);
 
     BotController(BotService botService) {
         this.botService = botService;
@@ -29,14 +29,22 @@ public class BotController {
 
     @PostMapping("/newCourtBooking/{playingDate}/{startTime}")
     public ResponseEntity<String> addBooking(@PathVariable("playingDate") String playingDate, @PathVariable("startTime") int startTime) {
-        LocalDate bookingDate = LocalDate.parse(playingDate).minusDays(7);
-        LocalDateTime bookingDateTime = bookingDate.atTime(00, 00, 00);
+        LocalDate transformedPlayingDate = LocalDate.parse(playingDate);
+        LocalDate bookingDate = transformedPlayingDate.minusDays(7);
+        LocalDateTime bookingDateTime = bookingDate.atTime(BOOKING_START_TIME);
         Date transformedBookingDate = Date.from(bookingDateTime.atZone(ZoneId.systemDefault()).toInstant());
         TimerTask courtBookingTask = new TimerTask() {
             @Override
             public void run() {
                 try {
-                    botService.bookCourt(startTime, bookingDate);
+                    LocalTime now = LocalTime.now();
+                    if (now.isAfter(BOOKING_END_TIME)) {
+                        cancel();
+                    }
+                    boolean isBooked = botService.bookCourt(startTime, transformedPlayingDate);
+                    if (isBooked) {
+                        cancel();
+                    }
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -44,7 +52,12 @@ public class BotController {
         };
         this.courtBookingTasks.put(playingDate, courtBookingTask);
         Timer timer = new Timer("BookingTimer");
-        timer.schedule(courtBookingTask, transformedBookingDate);
+        if (Duration.between(bookingDate.atStartOfDay(), transformedPlayingDate.atStartOfDay()).toDays() <= 7) {
+            LocalDateTime today = LocalDate.now().atTime(BOOKING_START_TIME);
+            timer.scheduleAtFixedRate(courtBookingTask, Date.from(today.atZone(ZoneId.systemDefault()).toInstant()), 300000);
+        } else {
+            timer.scheduleAtFixedRate(courtBookingTask, transformedBookingDate, 60000);
+        }
         return ResponseEntity.ok()
                 .body("Created booking task (" +
                         "PlayingDate: " + playingDate + ", " +
@@ -59,6 +72,15 @@ public class BotController {
         this.courtBookingTasks.remove(playingDate);
         return ResponseEntity.ok()
                 .body("Booking for " + playingDate + " canceled!");
+    }
+
+    @PostMapping("/updateBookingTimeWindow/{startTime}/{endTime}")
+    public ResponseEntity<String> updateBookingTimeWindow(@PathVariable("startTime") String startTime, @PathVariable("endTime") String endTime) {
+        System.out.println("Start: " + LocalTime.parse(startTime));
+        this.BOOKING_START_TIME = LocalTime.parse(startTime);
+        this.BOOKING_END_TIME = LocalTime.parse(endTime);
+        return ResponseEntity.ok()
+                .body("new start time: " + this.BOOKING_START_TIME + ", new end time: " + this.BOOKING_END_TIME);
     }
 
 }
