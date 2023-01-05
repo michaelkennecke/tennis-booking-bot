605a1740d869f967effdcd17bdecff290124b380
Michael Kennecke <mkennecke@trustdble.com>
2023-01-05 17:59:01 +0100

    Change waiting for the court table in booking-bot to fix the timeout issue

--- src/main/java/com/example/blauhofbot/Bot/BookingBot.java
+++ src/main/java/com/example/blauhofbot/Bot/BookingBot.java
@@ -1,6 +1,7 @@
 package com.example.blauhofbot.Bot;
 
 import com.example.blauhofbot.Model.Booking;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,7 +17,7 @@ public abstract class BookingBot {
         this.environment = environment;
         System.setProperty("webdriver.chrome.driver", environment.getProperty("chromium.webdriver.path"));
         this.chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
+        this.chromeOptions.addArguments("--headless");
     }
 
     public abstract boolean book(Booking booking);
